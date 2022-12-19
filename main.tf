be850391222944b10faa6f33b868b5468385cf5f
Alex Mello <39780604+alexmello82@users.noreply.github.com>
2022-12-19 13:44:32 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "create_vpc" {
-  source      = "/Users/alexmello/terraform-module/network"
+  source      = "./network"
   vpc_name    = "aula"
   subnet_name = "us-central1"
   cidr_range  = "10.0.0.0/8"
@@ -7,7 +7,7 @@ module "create_vpc" {
 }
 
 module "create_instance" {
-  source  = "/Users/alexmello/terraform-module/instance"
+  source  = "./instance"
   vm_name = "vm-aula"
   vm_type = "e2-micro"
   zone    = "us-central1-a"
